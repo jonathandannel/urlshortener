9b7042260937fe1c98481476a3bbc4743f8ce9b9
jonathan <usixjad@gmail.com>
2018-06-12 15:11:59 +0000

    application skeleton

--- main.js
+++ main.js
@@ -3,3 +3,18 @@ var app = express();
 var PORT = 8080;
 
 app.set("view engine", "ejs");
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+var inputUrls = Object.values(urlDatabase); //['lighthouselabs.ca', 'google.ca']
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
