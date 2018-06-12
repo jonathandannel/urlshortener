e10834c507c1ad6006a8a43a56b697d20cc8bd9a
jonathan <usixjad@gmail.com>
2018-06-12 17:21:09 +0000

    added show url pace

--- express_server.js
+++ express_server.js
@@ -6,20 +6,25 @@ app.set("view engine", "ejs");
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
+  "343n1hf": "http://www.help.net"
 };
-
-var inputUrls = Object.values(urlDatabase); //['lighthouselabs.ca', 'google.ca']
 
 app.get("/", (req, res) => {
   res.end("Hello!");
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase};
+  let entries = Object.entries(urlDatabase); //array of key/value
+  let templateVars = { urls: entries};
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: [req.params.id, urlDatabase[req.params.id]] };
+  console.log(templateVars);
+  res.render("urls_show", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
